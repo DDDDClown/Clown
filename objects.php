932b6e3229ea9b472694563ee74a0364df8fb4d1
DDDDClown <49257355+DDDDClown@users.noreply.github.com>
2019-05-08 23:24:42 +0800

    Update objects.php

--- objects.php
+++ objects.php
@@ -1,7 +1,7 @@
 <?php
 class text{
 	var $url;
-	var $title;  //var类变量声明
+	var $title;  //var（视为公有）类变量声明，public（公有），protected（受保护），private（私有）
 	
 	function __construct($par1,$par2){
 		$this->url = $par1;
