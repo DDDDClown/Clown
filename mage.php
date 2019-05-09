c45f753d76fac38cdec90e2a5f6f0d9eca72dbfa
DDDDClown <49257355+DDDDClown@users.noreply.github.com>
2019-05-09 09:44:39 +0800

    Update mage.php

--- mage.php
+++ mage.php
@@ -5,14 +5,14 @@
 <body>
 <?php
 print("文件：");
-print(__FILE__);
+print(__FILE__);//魔术变量  获取文件名
 print("<BR>\n");
-print("<hr>");
+print("<hr>");//插入水平线
 print("系统：");
-print(PHP_OS);
+print(PHP_OS);//获取操作系统
 print("<hr>");
 print("php版本：");
-print(PHP_VERSION)		
+print(PHP_VERSION) //获取php版本
 ?>
 		
 </body>
