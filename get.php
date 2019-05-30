cf7b7a3c0526fab0caa37fd02c12776e38714a1c
DDDDClown <49257355+DDDDClown@users.noreply.github.com>
2019-05-30 10:44:23 +0800

    Add files via upload

---
+++ get.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="{CHARSET}">
+		<title></title>
+	</head>
+	<body>
+	<form action="to.php" method="get">
+	名字<input type="text" name="a">
+	特点<input type="text" name="b">
+	<input type="submit" value="提交">
+	</form>	
+	</body>
+</html>
