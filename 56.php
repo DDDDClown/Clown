72fc241ab6cd3c532a3737682c968572d84e7b2f
DDDDClown <49257355+DDDDClown@users.noreply.github.com>
2019-05-30 15:11:18 +0800

    Add files via upload

---
+++ 56.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<meta charset="utf-8">
+<title></title>	
+</head>
+<body>
+<?php
+print("123456789 颠倒");
+print(strrev("123456789")."<hr>");
+print("小写:".strtolower("Hello World")."<hr>");
+print("大写:".strtoupper("Hello World")."<hr>");
+$text="     qwerty      "	;
+print("\"".trim($text)."\"");
+?>
+</body>
+</html>
