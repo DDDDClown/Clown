5c67c2015f374d080a79e619bd82dc2c63a85234
DDDDClown <49257355+DDDDClown@users.noreply.github.com>
2019-05-30 09:12:43 +0800

    Update biaodan.php

--- biaodan.php
+++ biaodan.php
@@ -1,19 +1,27 @@
-<html>
+<html>	
 <head>
 <meta charset="utf-8">
 <title></title>
+<style>
+.error{color:#ff0000;}	
+</style>	
 </head>
 <body>
 <?php
-$n= $e = $g = $c = $w= "";//设置空值
+$n= $e = $g = $c = $w= "";
+$nerr = $eerr= $gerr="";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
-	$n = test_input($_POST["name"]);
-	$e = test_input($_POST["email"]);
+	if(empty($_POST["name"]))//empty()函数检验是否提交
+	{$nerr="必须填写";}
+	else{$n = test_input($_POST["name"]);}
+	if(empty($_POST["email"])){$eerr="必须填写";}
+	else{$e = test_input($_POST["email"]);}
 	$w = test_input($_POST["website"]);
 	$c = test_input($_POST["comment"]);
-	$g = test_input($_POST["gender"]);
+	if(empty($_POST["gender"])){$gerr="必须选择";}
+	else{$g = test_input($_POST["gender"]);}
 }
 function test_input($data)
 {
@@ -26,13 +34,19 @@ function test_input($data)
 
 <h2>表单验证</h2>
 <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
-名字：<input type="text" name="name"><br><br>
-E-mail:<input type="text" name="email"><br><br>
+名字：<input type="text" name="name">
+	<span class="error">*<?php echo $nerr;?></span>//<span>提示标签
+	<br><br>
+E-mail:<input type="text" name="email">
+	<span class="error">*<?php echo $eerr;?></span>
+	<br><br>
 网址：<input type="text" name="website"><br><br>
 备注：<textarea name="comment" rows="5" cols="40"></textarea><br><br>
 性别：
 <input type="radio" name="gender" value="女">女
-<input type="radio" name="gender" value="男">男<br><br>
+<input type="radio" name="gender" value="男">男
+	<span class="error">*<?php echo $gerr;?></span>
+	<br><br>
 <input type="submit" name="submit" value="Submit">									
 </form>
 
@@ -49,5 +63,5 @@ echo "<br>";
 echo $g;			
 ?>
 </body>
-</html>			
+</html>				
 		
