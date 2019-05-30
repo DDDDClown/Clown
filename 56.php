0256bbeda1a23292dfc9903f83c15e45ba59c52d
DDDDClown <49257355+DDDDClown@users.noreply.github.com>
2019-05-30 15:22:34 +0800

    Update 56.php

--- 56.php
+++ 56.php
@@ -6,11 +6,11 @@
 <body>
 <?php
 print("123456789 颠倒");
-print(strrev("123456789")."<hr>");
-print("小写:".strtolower("Hello World")."<hr>");
-print("大写:".strtoupper("Hello World")."<hr>");
+print(strrev("123456789")."<hr>");//strrev使字符串颠倒
+print("小写:".strtolower("Hello World")."<hr>");//strtolower字符串转化为小写
+print("大写:".strtoupper("Hello World")."<hr>");//strtoupper字符串转化为大写
 $text="     qwerty      "	;
-print("\"".trim($text)."\"");
+print("\"".trim($text)."\"");//trim删除头尾空格
 ?>
 </body>
 </html>
