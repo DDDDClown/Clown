b8a5f9ca1384317f65e1a7c0e61cba0d3f55cdcf
DDDDClown <49257355+DDDDClown@users.noreply.github.com>
2019-06-13 08:26:24 +0800

    Add files via upload

---
+++ 63.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<mate charset="utf-8">
+<title></title>
+</head>
+<body>
+<?php
+print(microtime());
+sleep(10);
+print("<br>\n");
+print(microtime());
+usleep(30);
+print("<br>\n");
+print(microtime());		
+?>
+</body>
+</html>S
